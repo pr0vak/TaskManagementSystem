67d780680010e2a4a60f0a8ff6a91fd519b44c70
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-09-02 09:47:45 +0600

    008 Добавлиние Endpoint для удаления задачи по Id

--- Api/Controllers/TasksController.cs
+++ Api/Controllers/TasksController.cs
@@ -5,172 +5,217 @@ using TaskManagement.API.Data;
 using TaskManagement.API.ModelDto;
 using TaskManagement.API.Models;
 
-namespace TaskManagement.API.Controllers
+namespace TaskManagement.API.Controllers;
+
+public class TasksController : BaseController
 {
-    public class TasksController : BaseController
+    public TasksController(AppDbContext dbContext) : base(dbContext)
     {
-        public TasksController(AppDbContext dbContext) : base(dbContext)
-        {
-        }
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetTasks()
-        {
-            return Ok(await dbContext.ProjectTasks.ToListAsync());
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetTasks()
+    {
+        return Ok(await dbContext.ProjectTasks.ToListAsync());
+    }
 
-        [HttpGet("{id}", Name = nameof(GetTaskById))]
-        public async Task<IActionResult> GetTaskById(int id)
+    [HttpGet("{id}", Name = nameof(GetTaskById))]
+    public async Task<IActionResult> GetTaskById(int id)
+    {
+        if (id <= 0)
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Некорректный Id" }
+            });
+
+        var task = await dbContext.ProjectTasks
+            .FirstOrDefaultAsync(t => t.Id == id);
+
+        if (task is null)
+            return NotFound(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.NotFound,
+                ErrorMessages = { "Неверный Id" }
+            });
+
+        return Ok(new ServerResponse()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Result = task
+        });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ServerResponse>> CreateTask(
+        [FromBody] TaskCreateDto taskCreateDto)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (!Enum.IsDefined(taskCreateDto.Status))
+                {
+                    return BadRequest(new ServerResponse()
+                    {
+                        IsSuccess = false,
+                        StatusCode = HttpStatusCode.BadRequest,
+                        ErrorMessages = { "Некорректный статус задачи" }
+                    });
+                }
+
+                ProjectTask task = new()
+                {
+                    Title = taskCreateDto.Title,
+                    Description = taskCreateDto.Description,
+                    Status = taskCreateDto.Status
+                };
+
+                await dbContext.ProjectTasks.AddAsync(task);
+                await dbContext.SaveChangesAsync();
+
+                var response = new ServerResponse()
+                {
+                    StatusCode = HttpStatusCode.Created,
+                    Result = task
+                };
+
+                return CreatedAtRoute(nameof(GetTaskById), new { id = task.Id }, response);
+            }
+            else
+            {
+                return BadRequest(new ServerResponse()
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Модель данных не валидна" }
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Что-то пошло не так", ex.Message }
+            });
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ServerResponse>> UpdateTask(
+        int id, [FromBody] TaskUpdateDto taskUpdateDto)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (taskUpdateDto is null || taskUpdateDto.Id != id)
+                {
+                    return BadRequest(new ServerResponse()
+                    {
+                        IsSuccess = false,
+                        StatusCode = HttpStatusCode.BadRequest,
+                        ErrorMessages = { "Модель данных не валидна" }
+                    });
+                }
+
+                var taskFromDb = await dbContext.ProjectTasks.FindAsync(id);
+
+                if (taskFromDb is null)
+                {
+                    return NotFound(new ServerResponse()
+                    {
+                        IsSuccess = false,
+                        StatusCode = HttpStatusCode.NotFound,
+                        ErrorMessages = { "Задача не найдена" }
+                    });
+                }
+
+                if (!string.IsNullOrWhiteSpace(taskUpdateDto.Description))
+                {
+                    taskFromDb.Description = taskUpdateDto.Description;
+                }
+
+                taskFromDb.Title = taskUpdateDto.Title;
+                taskFromDb.Status = (Models.TaskStatus)taskUpdateDto.Status;
+
+                dbContext.ProjectTasks.Update(taskFromDb);
+                await dbContext.SaveChangesAsync();
+
+                return Ok(new ServerResponse()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Result = taskFromDb
+                });
+            }
+            else
+            {
+                return BadRequest(new ServerResponse()
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Модель данных не валидна" }
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Что-то пошло не так", ex.Message }
+            });
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ServerResponse>> RemoveTaskById(int id)
+    {
+        try
         {
             if (id <= 0)
+            {
                 return BadRequest(new ServerResponse()
                 {
                     IsSuccess = false,
                     StatusCode = HttpStatusCode.BadRequest,
                     ErrorMessages = { "Некорректный Id" }
                 });
+            }
 
-            var task = await dbContext.ProjectTasks
-                .FirstOrDefaultAsync(t => t.Id == id);
+            var task = await dbContext.ProjectTasks.FindAsync(id);
 
             if (task is null)
+            {
                 return NotFound(new ServerResponse()
                 {
                     IsSuccess = false,
                     StatusCode = HttpStatusCode.NotFound,
-                    ErrorMessages = { "Неверный Id" }
+                    ErrorMessages = { "Задача не найдена" }
                 });
+            }
+
+            dbContext.ProjectTasks.Remove(task);
+            await dbContext.SaveChangesAsync();
 
             return Ok(new ServerResponse()
             {
-                StatusCode = HttpStatusCode.OK,
-                Result = task
+                StatusCode = HttpStatusCode.NoContent
             });
         }
-
-        [HttpPost]
-        public async Task<ActionResult<ServerResponse>> CreateTask(
-            [FromBody] TaskCreateDto taskCreateDto)
+        catch (Exception ex)
         {
-            try
+            return BadRequest(new ServerResponse()
             {
-                if (ModelState.IsValid)
-                {
-                    if (!Enum.IsDefined(taskCreateDto.Status))
-                    {
-                        return BadRequest(new ServerResponse()
-                        {
-                            IsSuccess = false,
-                            StatusCode = HttpStatusCode.BadRequest,
-                            ErrorMessages = { "Некорректный статус задачи" }
-                        });
-                    }
-
-                    ProjectTask task = new()
-                    {
-                        Title = taskCreateDto.Title,
-                        Description = taskCreateDto.Description,
-                        Status = taskCreateDto.Status
-                    };
-
-                    await dbContext.ProjectTasks.AddAsync(task);
-                    await dbContext.SaveChangesAsync();
-
-                    var response = new ServerResponse()
-                    {
-                        StatusCode = HttpStatusCode.Created,
-                        Result = task
-                    };
-
-                    return CreatedAtRoute(nameof(GetTaskById), new { id = task.Id }, response);
-                }
-                else
-                {
-                    return BadRequest(new ServerResponse()
-                    {
-                        IsSuccess = false,
-                        StatusCode = HttpStatusCode.BadRequest,
-                        ErrorMessages = { "Модель данных не валидна" }
-                    });
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new ServerResponse()
-                {
-                    IsSuccess = false,
-                    StatusCode = HttpStatusCode.BadRequest,
-                    ErrorMessages = { "Что-то пошло не так", ex.Message }
-                });
-            }
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<ServerResponse>> UpdateTask(
-            int id, [FromBody] TaskUpdateDto taskUpdateDto)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    if (taskUpdateDto is null || taskUpdateDto.Id != id)
-                    {
-                        return BadRequest(new ServerResponse()
-                        {
-                            IsSuccess = false,
-                            StatusCode = HttpStatusCode.BadRequest,
-                            ErrorMessages = { "Модель данных не валидна" }
-                        });
-                    }
-
-                    var taskFromDb = await dbContext.ProjectTasks.FindAsync(id);
-
-                    if (taskFromDb is null)
-                    {
-                        return NotFound(new ServerResponse()
-                        {
-                            IsSuccess = false,
-                            StatusCode = HttpStatusCode.NotFound,
-                            ErrorMessages = { "Задача не найдена" }
-                        });
-                    }
-
-                    if (!string.IsNullOrWhiteSpace(taskUpdateDto.Description))
-                    {
-                        taskFromDb.Description = taskUpdateDto.Description;
-                    }
-
-                    taskFromDb.Title = taskUpdateDto.Title;
-                    taskFromDb.Status = (Models.TaskStatus)taskUpdateDto.Status;
-
-                    dbContext.ProjectTasks.Update(taskFromDb);
-                    await dbContext.SaveChangesAsync();
-
-                    return Ok(new ServerResponse()
-                    {
-                        StatusCode = HttpStatusCode.OK,
-                        Result = taskFromDb
-                    });
-                }
-                else
-                {
-                    return BadRequest(new ServerResponse()
-                    {
-                        IsSuccess = false,
-                        StatusCode = HttpStatusCode.BadRequest,
-                        ErrorMessages = { "Модель данных не валидна" }
-                    });
-                }
-            }
-            catch (Exception ex)
-            {
-                    return BadRequest(new ServerResponse()
-                    {
-                        IsSuccess = false,
-                        StatusCode = HttpStatusCode.BadRequest,
-                        ErrorMessages = { "Что-то пошло не так", ex.Message }
-                    });
-            }
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Что-то пошло не так", ex.Message }
+            });
         }
     }
 }
