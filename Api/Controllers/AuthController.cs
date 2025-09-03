2c5896d707d2b7f6fb80af210472ba07e40bea3a
Vyacheslav Kalin <slava93omsk@mail.ru>
2025-09-03 14:27:35 +0600

    010 Добавление модели регистрации. Добавление Endpoint регистрации.

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,21 +1,99 @@
+using System.Net;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TaskManagement.API.Data;
+using TaskManagement.API.ModelDto;
 using TaskManagement.API.Models;
 
-namespace TaskManagement.API.Controllers
-{
-    public class AuthController : BaseController
-    {
-        private readonly UserManager<AppUser> userManager;
-        private readonly RoleManager<IdentityRole> roleManager;
+namespace TaskManagement.API.Controllers;
 
-        public AuthController(AppDbContext dbContext,
-            UserManager<AppUser> userManager,
-            RoleManager<IdentityRole> roleManager)
-            : base(dbContext)
+public class AuthController : BaseController
+{
+    private readonly UserManager<AppUser> userManager;
+    private readonly RoleManager<IdentityRole> roleManager;
+
+    public AuthController(AppDbContext dbContext,
+        UserManager<AppUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+        : base(dbContext)
+    {
+        this.userManager = userManager;
+        this.roleManager = roleManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(
+        [FromBody] UserRegisterDto userRegisterDto)
+    {
+        if (userRegisterDto is null)
         {
-            this.userManager = userManager;
-            this.roleManager = roleManager;
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Некорректные данные" }
+            });
         }
+
+        var userFromDb = await dbContext.AppUsers
+            .FirstOrDefaultAsync(u => u.NormalizedUserName.Equals(
+                    userRegisterDto.UserName.ToUpper()));
+
+        if (userFromDb is not null)
+        {
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Пользователь с таким именем уже существует" }
+            });
+        }
+
+        var newAppUser = new AppUser
+        {
+            UserName = userRegisterDto.UserName,
+            FirstName = userRegisterDto.UserName,
+            Email = userRegisterDto.Email,
+            NormalizedEmail = userRegisterDto.Email.ToUpper()
+        };
+
+        var result = await userManager
+            .CreateAsync(newAppUser, userRegisterDto.Password);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(new ServerResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Ошибка регистрации" }
+            });
+        }
+
+        var newRoleAppUser = string.Empty;
+
+        if (SharedData.Roles.Admin.Equals(userRegisterDto.Role,
+            StringComparison.OrdinalIgnoreCase))
+        {
+            newRoleAppUser = SharedData.Roles.Admin;
+        }
+        else if (SharedData.Roles.Manager.Equals(userRegisterDto.Role,
+            StringComparison.OrdinalIgnoreCase))
+        {
+            newRoleAppUser = SharedData.Roles.Manager;
+        }
+        else
+        {
+            newRoleAppUser = SharedData.Roles.User;
+        }
+
+        await userManager.AddToRoleAsync(newAppUser, newRoleAppUser);
+
+        return Ok(new ServerResponse()
+        {
+            StatusCode = HttpStatusCode.Created,
+            Result = "Пользователь успешно зарегистрирован"
+        });
     }
 }
---
+++ Api/ModelDto/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+namespace TaskManagement.API.ModelDto;
+
+public class UserRegisterDto
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+}
